28d63fbba3b5067726bdf70504faf8175e478098
loglogin2 <i9991559@gmail.com>
2017-03-15 00:55:45 +0300

    Number system/feature: implement all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getBase(base) {
   // your solution
+    var check = 0;
+
+    if (base[0] === '1')
+        for(var i = 1; i < base.length; i++)
+            if(base[i] === '0')
+                check++;
+
+    if(check == base.length-1)
+        return base - 1;
+
+    var test_num = base;
+    var ret_base = 2;
+
+    while( test_num > ret_base )
+    {
+        if( test_num % ret_base == 1 )
+            test_num = Math.floor( test_num / ret_base );
+        else
+        {
+            test_num = base;
+            ret_base++;
+        }
+    }
+    return ret_base;
 }
